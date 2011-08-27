53b9013ea8c2bef845334fe2ab8d4bd6179da816
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-27 23:46:45 +0300

    Updated

--- website/app.js
+++ website/app.js
@@ -2,13 +2,8 @@
 
 var app = module.exports = {},
     config = require('./safe-config.js'),
-	website_app = require('./website_app.js'),
-	shell_app = require('./shell_app.js');
-
-website_app.config(config.website);
-shell_app.config(config.shell);
-
-website_app.listen( config.website );
-shell_app.listen( config.shell );
+	website_app = require('./website_app.js');
+website_app.config(config);
+website_app.listen(config);
 
 /* EOF */
