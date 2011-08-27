360a27dce52b963461cd84aaa0225e89d755c545
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-27 07:54:55 +0300

    Updated

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,19 @@
 /* Client Side JavaScript */
+
+/* Shorter format */
+function $(name) {
+	return document.getElementByID(name);
+}
+
+/* Init window */
+function init() {
+	var socket = io.connect('http://localhost');
+	socket.on('htmlmsg', function (data) {
+		//console.log(data);
+		$('rows').innerHTML += data;
+	});
+}
+
+window.onload = init;
+
+/* EOF */
