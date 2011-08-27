dd32f8b681aca87a8346f08a56f8d39202944bd9
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-27 08:47:13 +0300

    Updated

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,7 +7,8 @@ function $(name) {
 
 /* Init window */
 function init() {
-	var socket = io.connect('http://localhost:3000');
+	//var socket = io.connect('http://localhost:3000');
+	var socket = io.connect();
 	socket.on('htmlmsg', function (data) {
 		console.log('Writing to page!');
 		$('rows').innerHTML += data;
