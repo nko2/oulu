b7eeae438ce59d52be3964dd3a54bd707d5fe4c6
Paavo Pokkinen <paveq2@gmail.com>
2011-08-27 08:15:01 +0300

    using automatic address detection

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,7 +7,7 @@ function $(name) {
 
 /* Init window */
 function init() {
-	var socket = io.connect('http://localhost:3000');
+	var socket = io.connect();
 	socket.on('htmlmsg', function (data) {
 		//console.log(data);
 		$('rows').innerHTML += data;
