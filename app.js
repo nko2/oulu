d3bad427a52b23d752233338fa1b8c0dacb1ec64
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-27 07:38:38 +0300

    Updated

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ io.sockets.on('connection', function (socket) {
 	else app.listen(port);
 	
 	console.log("Express server listening on port %d in %s mode",
-		app.address().port, app.settings.env);
+		port, app.settings.env);
 
 })(); // end of setup HTTP
 
