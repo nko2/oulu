69db7f43f36f117819c5e18fa6f438efd069184a
Paavo Pokkinen <paveq2@gmail.com>
2011-08-27 12:51:13 +0300

    prototype and part of receiving msgs from client

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,14 +7,18 @@ function $(name) {
 
 /* Init window */
 function init() {
-	/*
+	
 	//var socket = io.connect('http://localhost:3000');
 	var socket = io.connect();
 	socket.on('htmlmsg', function (data) {
 		console.log('Writing to page!');
 		$('rows').innerHTML += data;
 	});
-	*/
+	
+}
+function sendMsg() {
+	
+	socket.emit('msg', document.sendmsgform.msg.value);
 }
 
 window.onload = init;
