4f2ce1204ffb928e84a5c51b716955d52de08011
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-27 11:11:53 +0300

    Updated

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,12 +7,14 @@ function $(name) {
 
 /* Init window */
 function init() {
+	/*
 	//var socket = io.connect('http://localhost:3000');
 	var socket = io.connect();
 	socket.on('htmlmsg', function (data) {
 		console.log('Writing to page!');
 		$('rows').innerHTML += data;
 	});
+	*/
 }
 
 window.onload = init;
