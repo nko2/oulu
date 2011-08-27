1220acf9e1f4d5ed316329d30e165259b9837005
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-28 00:45:00 +0300

    Updated

--- node-icecapd/icecapd.js
+++ node-icecapd/icecapd.js
@@ -1,23 +1,34 @@
 /* icecapd */
 
-var io = require('socket.io-client'),
-    socket = io.connect('http://localhost:3000/shell'),
+var init = require('init'),
     sys = require('sys'),
+    config = require('./config.js'),
+    pidfile = config.pidfile || './myprog.pid',
+    logfile = config.logfile,
+    io = require('socket.io-client'),
+    socket = io.connect('http://localhost:3000/shell'),
     icecap = require('icecap').create();
 
-socket.on('connect', function () {
-	console.log('DEBUG: Connection !');
+init.simple({
+	pidfile : pidfile,
+	logfile : logfile,
+	command : process.argv[3],
+	run     : function () {
+		socket.on('connect', function () {
+			console.log('DEBUG: Connection !');
+			
+			icecap.on('event', function(name, tokens) {
+				console.log("DEBUG: icecap.on(event): " + sys.inspect(name) + ": " + sys.inspect(tokens) );
+				socket.emit('icecap-event', name, tokens);
+			});
+			
+			socket.on('icecap.command', function(name, tokens) {
+				console.log("DEBUG: socket.on(client-event): " + sys.inspect(name) + ": " + sys.inspect(tokens) );
+				icecap.command(name, tokens);
+			});
 	
-	icecap.on('event', function(name, tokens) {
-		console.log("DEBUG: icecap.on(event): " + sys.inspect(name) + ": " + sys.inspect(tokens) );
-		socket.emit('icecap-event', name, tokens);
-	});
-	
-	socket.on('icecap.command', function(name, tokens) {
-		console.log("DEBUG: socket.on(client-event): " + sys.inspect(name) + ": " + sys.inspect(tokens) );
-		icecap.command(name, tokens);
-	});
-	
+		});
+	}
 });
 
 /* EOF */
