f9dc75cf5c543ac724686d90f8aa32661fe95f8f
Juho Juopperi <jkj@kapsi.fi>
2011-08-28 09:30:10 -0400

    nextuid couchdb query

---
+++ website/tests/nextuid.js
@@ -0,0 +1,12 @@
+var cradle = require('cradle');
+var db = new(cradle.Connection)().database('users');
+
+//
+// Query users/list which returns all api keys
+//
+db.view('users/nextuid', function (err, res) {
+	res.forEach(function(next) {
+		console.log('next: '+next);
+	});
+});
+
