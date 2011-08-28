ab079daf6077f196c75d6fefdd89bb1b3f821397
Paavo Pokkinen <paveq2@gmail.com>
2011-08-28 07:20:27 -0400

    helpers.js

--- website/public/javascripts/client.js
+++ website/public/javascripts/client.js
@@ -50,23 +50,6 @@ function init() {
 	});*/
 }
 
-// Convert unix-timestamp to HH:MM format
-function HHmm(time) {
-	var dt = new Date(time * 1000);
-	
-	var hours = dt.getHours();
-	var minutes = dt.getMinutes();
-	
-	if ( minutes < 10) {
-		minutes = "0"+minutes;
-	};
-	if ( hours < 10) {
-		hours = "0"+hours;
-	};
-	
-	return hours +":"+ minutes;
-};
-
 // jquery
 $(document).ready(function() {
 	init();	
