6a9e989f3119b5345d9d1c882c5d0c7c8cd0e10b
Paavo Pokkinen <paveq2@gmail.com>
2011-08-28 16:30:04 -0400

    purkka

--- website/public/javascripts/client.js
+++ website/public/javascripts/client.js
@@ -51,27 +51,47 @@ function init() {
 	// receive line from IRC
 	socket.on('icecap-event', function (name, data) {
 		console.log("icecap-event received: '" + name + "'");
-		if(name !== 'msg') return;
+		if(name == 'msg') {
 		
-		get_avatar(data['address'], function(url, bigurl) {
+			get_avatar(data['address'], function(url, bigurl) {
+				function escape(str) { return $('<span/>').text(str).html(); }
+				function img() {
+					if(url) return '<a class="imgurl" href="'+bigurl+'"><img src="'+url+'" title="'+escape(data['address'])+'"/></a>';
+					return '';
+				}
+				$('#ircrows').prepend('<div class="ircrow" style="display: none;">'+img()+' '+
+					escape(HHmm(data.time))+
+					' &lt;'+
+					escape(data.presence)+
+					'&gt; '+
+					make_urls(escape(data.msg))+
+					'<hr/></div>');
+				$('.ircrow').fadeIn('slow');
+				if(url) $('.imgurl').imgPreview({ imgCSS: { width: 200 } });
+				if (data.msg.match(/(.*).(jpg|gif|jpeg|png)$/)) {
+					$('.imgurl').imgPreview({ imgCSS: { width: 200 } });
+				};
+			});
+		};
+		if(name == 'channel_presence_added') {
 			function escape(str) { return $('<span/>').text(str).html(); }
-			function img() {
-				if(url) return '<a class="imgurl" href="'+bigurl+'"><img src="'+url+'" title="'+escape(data['address'])+'"/></a>';
-				return '';
-			}
-			$('#ircrows').prepend('<div class="ircrow" style="display: none;">'+img()+' '+
-				escape(HHmm(data.time))+
-				' &lt;'+
-				escape(data.presence)+
-				'&gt; '+
-				make_urls(escape(data.msg))+
-				'<hr/></div>');
+			$('#ircrows').prepend('<div class="ircrow" style="display: none; margin-left: 36px;">'+
+                    escape(HHmm(data.time))+ ' '+
+                    escape(data.presence)+
+                    ' joined'+
+                    '<hr/></div>');
 			$('.ircrow').fadeIn('slow');
-			if(url) $('.imgurl').imgPreview({ imgCSS: { width: 200 } });
-			if (data.msg.match(/(.*).(jpg|gif|jpeg|png)$/)) {
-				$('.imgurl').imgPreview({ imgCSS: { width: 200 } });
-			};
-		});
+		};
+		if(name == 'channel_presence_removed') {
+			function escape(str) { return $('<span/>').text(str).html(); }
+			$('#ircrows').prepend('<div class="ircrow" style="display: none; margin-left: 36px;">'+
+                    escape(HHmm(data.time))+ ' '+
+                    escape(data.presence)+
+                    ' left'+
+                    '<hr/></div>');
+			$('.ircrow').fadeIn('slow');
+		};
+
 	});
 	
 	// send line to IRC
